c430fe4d2bfabe6690c22471d417ee5b4baf3361
jeremy_wei92 <jeremy_wei92@outlook.com>
2024-02-26 14:20:43 +0800

    changes

---
+++ voucher_system/api_voucher_system/Context/Migrations/20240126015759_InitialMigration1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api_voucher_system.Context.Migrations
+{
+    public partial class InitialMigration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
